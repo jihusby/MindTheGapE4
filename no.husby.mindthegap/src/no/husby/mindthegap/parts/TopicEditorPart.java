1b86c46ec23edda0e26780eaee496c9dbb9fd3a1
Jan Inge Husby <jihusby@gmail.com>
2013-04-08 22:15:11 +0200

    Fix style - first steps

--- no.husby.mindthegap/src/no/husby/mindthegap/parts/TopicEditorPart.java
+++ no.husby.mindthegap/src/no/husby/mindthegap/parts/TopicEditorPart.java
@@ -20,24 +20,35 @@ public class TopicEditorPart {
 
 	@PostConstruct
 	public void init(Composite parent, @Named(IServiceConstants.ACTIVE_SELECTION) Topic item) {
-		GridLayout layout = new GridLayout(2, true);
+		GridLayout layout = new GridLayout(2, false);
 		parent.setLayout(layout);
 
 		Label titleLabel = new Label(parent, SWT.NONE);
-		titleLabel.setLayoutData(new GridData(SWT.LEFT, SWT.TOP, true, false, 1, 1));
+		titleLabel.setLayoutData(new GridData(SWT.LEFT, SWT.TOP, false, false, 1, 1));
 		titleLabel.setText("Topic :");
 
-		Label recordLabel = new Label(parent, SWT.NONE);
-		recordLabel.setLayoutData(new GridData(SWT.LEFT, SWT.TOP, true, false, 1, 2));
-		recordLabel.setText("Records :");
-
 		titleField = new Text(parent, SWT.BORDER);
-		titleField.setLayoutData(new GridData(SWT.LEFT, SWT.TOP, true, false, 2, 1));
+		titleField.setLayoutData(new GridData(SWT.FILL, SWT.TOP, true, false, 1, 1));
 		titleField.setText(item.getTitle());
 
+		Label recordLabel = new Label(parent, SWT.NONE);
+		recordLabel.setLayoutData(new GridData(SWT.LEFT, SWT.TOP, false, false, 1, 1));
+		recordLabel.setText("Records :");
+
 		recordField = new Text(parent, SWT.BORDER);
-		recordField.setLayoutData(new GridData(SWT.LEFT, SWT.TOP, true, false, 2, 2));
+		recordField.setLayoutData(new GridData(SWT.FILL, SWT.TOP, true, false, 1, 1));
 		recordField.setText(((Integer) item.getRecords()).toString());
+
+		// Text extraText = new Text(parent, SWT.NONE);
+		// extraText.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true,
+		// 1, 2));
+		// extraText
+		// .setText("In my try to move an 3.x RCP Application to 4 I managed to bump into another issue. "
+		// +
+		// "I created a simple test part with a simple Text on it. When I start the application, "
+		// +
+		// " it displays fine and does not show any errors. But when I try typing in the field I get a null pointer in the KeyBindingDispatcher.");
+
 	}
 
 	@Focus
